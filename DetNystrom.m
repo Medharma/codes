2fc0d9eae54521dad3d48414569b36aae5a0e439
Medharma <medhasharma3250@gmail.com>
2017-02-15 09:42:25 +0530

    Revert "RF energy harvesting wireless communication part 1"
    This reverts commit ea83864bbdee3f28bc4fbc652cc738df5d0b05ba.

--- DetNystrom.m
+++
@@ -1,8 +0,0 @@
-%% Function for Fredholm determinant
-
-function deter = DetNystrom(K,z,a,b,m)
-[w,y] = quad(m,a,b); % w = quad(m,a,b)
-w = sqrt(w); 
-[yj,yi] = meshgrid(y,y);
-deter = det(eye(m)+z*(w'*w).*K(yi,yj));
-end
